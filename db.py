4a68aff7d96fdda2f52434f0e092ca26c2fa1555
aljt86 <aljt86@gmail.com>
2026-05-04 11:59:36 -0500

    Configuración inicial de conexión a SQL Server (db.py)

---
+++ db.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+server = "localhost"
+database = "documentos_sena"
+username = "tu_usuario"
+
+connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
+
+engine = create_engine(conection_string)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally: 
+        db.close()
