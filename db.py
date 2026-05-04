d98fc9171aec14761b97b8ff8acc3b6d703476f9
aljt86 <aljt86@gmail.com>
2026-05-04 12:03:41 -0500

    Update db.py

--- db.py
+++ db.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 server = "localhost"
 database = "documentos_sena"
 username = "tu_usuario"
+password = "tu_contraseña"
 
 connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
 
