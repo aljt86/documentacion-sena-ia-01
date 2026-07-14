ed4f3f74f6fc6db82bb8981fdf4b9c298e20ee12
Haic <haic@ejemplo.com>
2026-07-14 08:30:18 -0500

    Configuración inicial Alembic y conexión DB

--- alembic/env.py
+++ alembic/env.py
@@ -3,27 +3,12 @@ from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 from api.db import Base
 
-
 config = context.config
 
-import os
-
-database_url = os.getenv("DATABASE_URL")
-if database_url:
-    config.set_main_option("sqlalchemy.url", database_url)
-
-fileConfig(config.config_file_name)
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = Base.metadata 
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
