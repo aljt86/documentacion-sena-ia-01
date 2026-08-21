667ec5edf1ce2553f17b199bbde945ecaf0bbc1d
Haic <haic@ejemplo.com>
2026-08-21 14:38:39 -0500

    Ajuste de coordenadas en zones_hologramas

--- app/ocr_template.py
+++ app/ocr_template.py
@@ -75,9 +75,9 @@ def _obtener_url_crop(file_path):
 # ============================================================
 
 zones_hologramas_anverso = {
-    "numero_documento": (0.04, 0.16, 0.45, 0.19),
-    "apellidos":        (0.04, 0.19, 0.45, 0.26),
-    "nombres":          (0.04, 0.27, 0.45, 0.36),
+    "numero_documento": (0.04, 0.26, 0.85, 0.36),
+    "apellidos":        (0.04, 0.34, 0.50, 0.44),
+    "nombres":          (0.04, 0.46, 0.99, 0.60),
 }
 
 zones_hologramas_reverso = {
