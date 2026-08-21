bd1ea4ec23cc3c964ddbe584628235b73842746d
Haic <haic@ejemplo.com>
2026-08-21 17:40:05 -0500

    Ajustes en zones_hologramas_reverso

--- app/ocr_template.py
+++ app/ocr_template.py
@@ -81,10 +81,10 @@ zones_hologramas_anverso = {
 }
 
 zones_hologramas_reverso = {
-    "fecha_nacimiento": (0.32, 0.05, 0.75, 0.09),
-    "lugar_nacimiento": (0.32, 0.08, 0.55, 0.16),
-    "tipo_sangre":      (0.50, 0.16, 0.57, 0.22),
-    "sexo":             (0.63, 0.16, 0.70, 0.22),
+    "fecha_nacimiento": (0.45, 0.07, 0.72, 0.14),
+    "lugar_nacimiento": (0.10, 0.07, 0.42, 0.16),
+    "tipo_sangre":      (0.48, 0.16, 0.62, 0.23),
+    "sexo":             (0.68, 0.16, 0.78, 0.23),
 }
 
 zones_digital = {
